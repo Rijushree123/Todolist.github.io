d02590980bc8372d6497b41d8480153ef4e179c2
Rijushree007 <60056104+Rijushree123@users.noreply.github.com>
2023-02-09 23:09:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,96 +10,56 @@ input.addEventListener('keypress',function(e){
 });
 function addItem(e){
   
-  const item  = input.value;
-  const task_el = document.createElement('div');
-		task_el.classList.add('task');
+  const item_value  = input.value;
+  const item = document.createElement('div');
+		item.classList.add('item');
 
-		const task_content_el = document.createElement('div');
-		task_content_el.classList.add('content');
+		const item_content = document.createElement('div');
+		item_content.classList.add('content');
 
-		task_el.appendChild(task_content_el);
+		item.appendChild(item_content);
 
-		const task_input_el = document.createElement('input');
-		task_input_el.classList.add('text');
-		task_input_el.type = 'text';
-		task_input_el.value = item;
-		task_input_el.setAttribute('readonly', 'readonly');
-        task_input_el.addEventListener('dblclick', function(){
-            task_input_el.style.textDecoration = "line-through";
+		const input_item = document.createElement('input');
+		input_item.classList.add('text');
+		input_item.type = 'text';
+		input_item.value = item_value;
+		input_item.setAttribute('readonly', 'readonly');
+        input_item.addEventListener('dblclick', function(){
+            input_item.style.textDecoration = "line-through";
         })
-		task_content_el.appendChild(task_input_el);
+		item_content.appendChild(input_item);
 
-		const task_actions_el = document.createElement('div');
-		task_actions_el.classList.add('actions');
+		const item_action = document.createElement('div');
+		item_action.classList.add('actions');
 		
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit','btn','btn-success');
-		task_edit_el.type="button";
-		task_edit_el.innerText = 'Edit';
+		const edit_item = document.createElement('button');
+		edit_item.classList.add('edit','btn','btn-success');
+		edit_item.type="button";
+		edit_item.innerText = 'Edit';
 
-		const task_delete_el = document.createElement('button');
-		task_delete_el.classList.add('delete','btn','btn-danger','fa','fa-trash');
+		const delete_item = document.createElement('button');
+		delete_item.classList.add('delete','btn','btn-danger','fa','fa-trash');
 
-		task_actions_el.appendChild(task_edit_el);
-		task_actions_el.appendChild(task_delete_el);
+		item_action.appendChild(edit_item);
+		item_action.appendChild(delete_item);
 
-		task_el.appendChild(task_actions_el);
+		item.appendChild(item_action);
 
-		toDoContainer.appendChild(task_el);
+		toDoContainer.appendChild(item);
 
 		input.value = '';
-    task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
+    edit_item.addEventListener('click', (e) => {
+			if (edit_item.innerText.toLowerCase() == "edit") {
+				edit_item.innerText = "Save";
+				input_item.removeAttribute("readonly");
+				input_item.focus();
 			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
+				edit_item.innerText = "Edit";
+				input_item.setAttribute("readonly", "readonly");
 			}
 		});
 
-		task_delete_el.addEventListener('click', (e) => {
-			toDoContainer.removeChild(task_el);
+		delete_item.addEventListener('click', (e) => {
+			toDoContainer.removeChild(item);
 		});
 }
-//console.log(inputtext.innerHTML);
-
-/*var deleteBtn = document.createElement('button');
-
-// Add classes to del button
-deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-// Append text node
-deleteBtn.appendChild(document.createTextNode('X'));
-
-
-add.addEventListener('click',addItem);
-inputtext.addEventListener('keypress',function(e){
-    if(e.key=="Enter"){
-        addItem();
-    }
-});
-function addItem(){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputtext.value;
-    paragraph.appendChild(deleteBtn);
-    toDoContainer.appendChild(paragraph);
-    //console.log(paragraph.innerText);
-    inputtext.value='';
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    deleteBtn.addEventListener('click', removeItem);
-    function removeItem(e){
-    if(e.target.classList.contains('delete')){
-      if(confirm('Are You Sure?')){
-        var li = e.target.parentElement;
-        toDoContainer.removeChild(paragraph);
-      }
-    }
-  }
-}
-
-*/
